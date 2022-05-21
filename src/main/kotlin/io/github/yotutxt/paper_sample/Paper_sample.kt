e548bbdd788f79ac130abce573458bc83c99df2f
q0v4 <yotu4gure@gmail.com>
2022-05-22 00:36:57 +0900

    Initial commit

---
+++ src/main/kotlin/io/github/yotutxt/paper_sample/Paper_sample.kt
@@ -0,0 +1,13 @@
+package io.github.yotutxt.paper_sample
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Paper_sample : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
